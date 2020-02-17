741d915e99efdca5b05de0730c8a98cb931a2656
rafongol <nadimouertani@hotmail.com>
2020-02-17 14:27:05 +0400

    Remove Event Schema

--- server.js
+++ server.js
@@ -15,39 +15,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful.'));
 
-const eventSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'An event must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'An envent must have a price']
-  }
-});
-
-const Event = mongoose.model('Event', eventSchema);
-
-const testEvent = new Event({
-  name: 'BQQ VOX CINEMA',
-  rating: 4.8,
-  price: 220
-});
-
-testEvent
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log(`Error: `, err);
-  });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}`);
